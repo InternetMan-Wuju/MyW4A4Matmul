a620334198818b819e9ff9071334994774950be3
root <root@nb-a17795513063829504970944-a17795513063829504970944-0.nb-a17795513063829504970944.hero-user.svc.cluster.local>
2025-11-20 13:40:17 +0000

    remove unnessensary files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ReadMe
-目前已经可以正常运行
+
+算子由 matmul_leakyrelu_custom 改装而来，https://gitee.com/ascend/samples/tree/v1.8-8.3.RC1.alpha002/operator/ascendc/0_introduction/13_matmulleakyrelu_kernellaunch/MatmulLeakyReluInvocation
+
+仅单核，一次性运算完成。
+
+关于main.cpp和script文件夹下的修改不再赘述
+在tiling.cpp和.cpp里添加注释便于理解
+
--- W4A4GroupMatmul_custom_tiling.cpp
+++ W4A4GroupMatmul_custom_tiling.cpp
@@ -23,97 +23,65 @@ using namespace std;
   */
 void GenerateTilingMatmul(const char *socVersion, uint8_t *tilingBuf)
 {
-  //未来需要支持动态Shape
-  //No tiling for now , 25.11.4
+
+    //静态定义矩阵大小，且不作切分，一次性运算完成
     constexpr int32_t M = 64;
     constexpr int32_t K = 32;
     constexpr int32_t N = 16;
-    
-    //int E = 4;?
 
+    //左矩阵定义
     TPosition XPosition = TPosition::VECOUT;
     CubeFormat XFormat = CubeFormat::ND;
     DataType XDtype = DataType::DT_INT8;
     bool isTransX = false;
 
+    //右矩阵定义
     TPosition WPosition = TPosition::VECOUT;
     CubeFormat WFormat = CubeFormat::ND;
     DataType WDtype = DataType::DT_INT8;
     bool isTransW = false;
 
-
-    // TPosition GroupListPosition = TPosition::GM;
-    // CubeFormat GroupListFormat = CubeFormat::ND;
-    // DataType GroupListDtype = DataType:::DT_INT64;
-    // bool isTransGroupList = false;
-
-    // TPosition YPosition = TPosition::GM;
-    // CubeFormat YFormat = CubeFormat::ND;
-    // DataType YDtype = DataType:::DT_FLOAT16;
-    //bool isTransY = false;
-
+    //结果矩阵定义
     TPosition MatmulPosition = TPosition::VECIN;
     CubeFormat MatmulFormat = CubeFormat::ND;
     DataType MatmulDtype = DataType::DT_INT32;
-    //bool isTransMatmul = false;
 
-    // TPosition biasPosition = TPosition::GM;
-    // CubeFormat biasFormat = CubeFormat::ND;
-    // DataType biasDtype = DataType::DT_FLOAT;
+    //我们没有bias
     bool isBias = false;
 
-    //int usedCoreNum = 1;
-    //先用单核吧
-    // int baseM = 64;
-    // int baseN = 32;
 
     optiling::TCubeTiling tilingData;
     auto ascendcPlatform = platform_ascendc::PlatformAscendCManager::GetInstance(socVersion);
     MatmulApiTiling tilingApi(*ascendcPlatform);
 
-    //tilingApi.SetDim(usedCoreNum); // Set the number of cores that participate in multi-core computaion is 2.
     tilingApi.SetAType(XPosition, XFormat, XDtype, isTransX);
     tilingApi.SetBType(WPosition, WFormat, WDtype, isTransW);
     tilingApi.SetCType(MatmulPosition, MatmulFormat, MatmulDtype);
-    //No bias Now
-    //tilingApi.SetBiasType(biasPosition, biasFormat, biasDtype);
+
 
     tilingApi.SetOrgShape(M, N, K);  
     tilingApi.SetShape(M, N, K); 
-    //tiling.SetSingleShape(M, N, K);?
     tilingApi.SetBias(isBias);
-    //No tiling now
-    //tilingApi.SetTraverse(MatrixTraverse::FIRSTM); // Set the matmul travse is FIRSTM.
-    //tilingApi.SetFixSplit(baseM, baseN, -1); // Set the fixed baseM=128, baseN=256.
     tilingApi.SetBufferSpace(-1, -1, -1);
 
-    int64_t step1MulRes = tilingApi.GetTiling(tilingData); // Get matmul tiling data.
-    //No tiling now
-    // tilingData.set_stepM(1); // Set the matmul tiling stepM=1.
-    // tilingData.set_stepN(1); // Set the matmul tiling stepN=1.
-    if (step1MulRes == -1) {
+    int64_t step1Mul = tilingApi.GetTiling(tilingData);
+
+    if (step1Mul == -1) {
         std::cout << "gen tiling failed, this code is from tiling.cpp" << std::endl;
     }
-    uint32_t tcubeTilingSize = tilingData.GetDataSize() + sizeof(uint64_t);
+    uint32_t tcubeTilingSize = tilingData.GetDataSize();
     tilingData.SaveToBuffer(tilingBuf, tcubeTilingSize);
-    //uint64_t localMemSize;
-    //ascendcPlatform->GetCoreMemSize(platform_ascendc::CoreMemType::UB, localMemSize);
-    //*reinterpret_cast<uint64_t *>(tilingBuf + tcubeTilingSize) = localMemSize;
+
     return;
 }
 
 void GenerateTilingScamul(const char *socVersion, uint8_t *tilingBuf)
 {
-  //未来需要支持动态Shape
-  //这里先直接全怼进去
-  //Scamul:那个x_scale和w_scale相乘的操作，相当于vector mul,不过我们还是直接用matmul，后面再调,25.11.4
+
     constexpr int32_t M = 64;
     constexpr int32_t K = 1;
     constexpr int32_t N = 32;
     
-    //int E = 4;
-
-
     TPosition xScalePosition = TPosition::VECOUT;
     CubeFormat xScaleFormat = CubeFormat::ND;
     DataType xScaleDtype = DataType::DT_FLOAT;
@@ -127,55 +95,27 @@ void GenerateTilingScamul(const char *socVersion, uint8_t *tilingBuf)
     TPosition ScamulPosition = TPosition::VECIN;
     CubeFormat ScamulFormat = CubeFormat::ND;
     DataType ScamulDtype = DataType::DT_FLOAT;
-    //bool isTransScamul = false;
-    // TPosition GroupListPosition = TPosition::GM;
-    // CubeFormat GroupListFormat = CubeFormat::ND;
-    // DataType GroupListDtype = DataType:::DT_INT64;
-    // bool isTransGroupList = false;
 
-    // TPosition YPosition = TPosition::GM;
-    // CubeFormat YFormat = CubeFormat::ND;
-    // DataType YDtype = DataType:::DT_FLOAT16;
-    // bool isTransY = false;
-
-
-    // TPosition biasPosition = TPosition::GM;
-    // CubeFormat biasFormat = CubeFormat::ND;
-    // DataType biasDtype = DataType::DT_FLOAT;
     bool isBias = false;
-
-    //int usedCoreNum = 1;
-    //先用单核吧
-    // int baseM = 64;
-    // int baseN = 32;
 
     optiling::TCubeTiling tilingData;
     auto ascendcPlatform = platform_ascendc::PlatformAscendCManager::GetInstance(socVersion);
     MatmulApiTiling tilingApi(*ascendcPlatform);
 
-    //tilingApi.SetDim(usedCoreNum); // Set the number of cores that participate in multi-core computaion is 2.
     tilingApi.SetAType(xScalePosition, xScaleFormat, xScaleDtype, isTransxScale);
     tilingApi.SetBType(wScalePosition, wScaleFormat, wScaleDtype, isTranswScale);
     tilingApi.SetCType(ScamulPosition, ScamulFormat, ScamulDtype);
-    //tilingApi.SetBiasType(biasPosition, biasFormat, biasDtype);
-
     tilingApi.SetOrgShape(M, N, K);
     tilingApi.SetShape(M, N, K);   
     tilingApi.SetBias(isBias);
-    //tilingApi.SetTraverse(MatrixTraverse::FIRSTM); // Set the matmul travse is FIRSTM.
-    //tilingApi.SetFixSplit(baseM, baseN, -1); // Set the fixed baseM=128, baseN=256.
     tilingApi.SetBufferSpace(-1, -1, -1);
 
-    int64_t step1MulRes = tilingApi.GetTiling(tilingData); // Get matmul tiling data.
-    // tilingData.set_stepM(1); // Set the matmul tiling stepM=1.
-    // tilingData.set_stepN(1); // Set the matmul tiling stepN=1.
-    if (step1MulRes == -1) {
+    int64_t step2Sca = tilingApi.GetTiling(tilingData);
+    if (step2Sca == -1) {
         std::cout << "gen tiling failed, this code is from tiling.cpp" << std::endl;
     }
     uint32_t tcubeTilingSize = tilingData.GetDataSize();
     tilingData.SaveToBuffer(tilingBuf, tcubeTilingSize);
 
-    //uint64_t localMemSize;
-    //ascendcPlatform->GetCoreMemSize(platform_ascendc::CoreMemType::UB, localMemSize);
     return;
 }
